706db22279495a3f88d006bfd324cf7bd7f4378f
Pasupathy Devaraj <115683488+PasupathyDeva@users.noreply.github.com>
2022-10-19 16:47:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,3 +38,6 @@ my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+streamlit.text("Thanks for adding " + add_my_fruit)
